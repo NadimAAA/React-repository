e60c474c17c7238efd6ee8b2694be72e7d1314da
Nadim Abou Ali <nadimabouali@Nadims-MacBook-Air.local>
2024-11-12 14:25:10 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Lorem</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias provident
+        voluptate nesciunt quidem, quisquam sed fugiat incidunt quos velit esse
+        neque dolorum hic inventore tempore, pariatur fugit ipsa nihil
+        reiciendis?
+      </p>
+    </div>
+  );
+};
+
+export default App;
